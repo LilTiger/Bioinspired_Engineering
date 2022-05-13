2273b54e675d5bdba424c73f1c9032e9e8f5d237
X Venom <anjm888@126.com>
2022-05-13 17:33:56 +0800

    update.

--- Machine_Learning/contribution.py
+++ Machine_Learning/contribution.py
@@ -7,15 +7,13 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestClassifier
-from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import f_classif, chi2, mutual_info_classif, f_regression
 import csv
 from sklearn import preprocessing
 
 csv_file = open('parameters.csv', 'r')
 reader = csv.reader(csv_file)
 
-# 从csv文件中读取参数和标签 声明两个空列表先
-# 读取到的parameters.csv为 正则化后的数据形式：第一列为索引 之后的列为 自变量-数据 自成一列
 x_label = []
 y_label = []
 for item in reader:
@@ -29,8 +27,9 @@ for item in reader:
 # clf控制台中的coef_和intercept分别为权重 w b
 # 注意 二分类问题中输出的仅为第二类的 w b
 clf = LogisticRegression()
-x = np.array(x_label)
-y = np.array(y_label)
+# 习惯将数据转化为 float 格式
+x = np.array(x_label).astype(np.float64)
+y = np.array(y_label).astype(np.float64)
 clf.fit(x, y)
 # decision_function返回结果的数值表示模型预测样本属于某个类别的可信度
 # 在二分类中表示positive正样本的可信度：大于0表示正样本的可信度大于负样本，否则可信度小于负样本
@@ -43,26 +42,26 @@ print(clf.predict_proba(ys))
 print(clf.predict(ys))
 
 
-# # 以下开始相关性计算(排除第一列 ref的影响 ref的作用仅仅是对比文献提取数据的准确性)
-# csv_data = pd.read_csv("parameters.csv")
-# # 若是计算parameters_norm.csv 注释掉下面两行即可
-# csv_data.drop(['ref'], axis=1, inplace=True)
-# csv_data.dropna(axis=1, inplace=True)
-# # 计算每个自变量的 相关系数
-# corr = csv_data.corr()
-# corr.to_csv('correlation.csv')
-# # 计算每个自变量的 f_score p_score 前者越大/后者越小 那么与因变量的关系越大 即该自变量越重要
-# f_score, p_score = f_classif(x_label, y_label)
-# print('f score: ', f_score)
-# print('p_score: ', p_score)
-# # 以下操作将f_score值转化为百分比 即可近似 特征（自变量）的【贡献度】
-# # 保存原数组sum 以免index的迭代中 sum会不断更新 造成不准确
-# sums = f_score.sum()
-# print(f'contribution rates of {len(x_label[0])} features are:')
-# parameters = ['Culture serum', 'Stretching direction', 'Cylic or static stretching', 'Continuous or intermittent',
-#               'Amplitude', 'Effective stretching duration']
-# for index in range(0, len(f_score)):
-#     f_score[index] /= sums
-#     # 此种百分比控制输出需掌握
-#     print(f'{parameters[index]}: {100*f_score[index]:.2f}% ')
+# 以下开始相关性计算(排除第一列 ref的影响 ref的作用仅仅是对比文献提取数据的准确性)
+csv_data = pd.read_csv("parameters.csv")
+# 若是计算parameters_norm.csv 注释掉下面两行即可
+csv_data.drop(['ref'], axis=1, inplace=True)
+csv_data.dropna(axis=1, inplace=True)
+# 计算每个自变量的 相关系数
+corr = csv_data.corr()
+corr.to_csv('correlation.csv')
+# 计算每个自变量的 f_score p_score 前者越大/后者越小 那么与因变量的关系越大 即该自变量越重要
+f_score, p_score = f_regression(x, y)
+print('f score: ', f_score)
+print('p_score: ', p_score)
+# 以下操作将f_score值转化为百分比 即可近似 特征（自变量）的【贡献度】
+# 保存原数组sum 以免index的迭代中 sum会不断更新 造成不准确
+sums = f_score.sum()
+print(f'contribution rates of {len(x_label[0])} features are:')
+parameters = ['Culture serum', 'Stretching direction', 'Cylic or static stretching', 'Continuous or intermittent',
+              'Amplitude', 'Effective stretching duration']
+for index in range(0, len(f_score)):
+    f_score[index] /= sums
+    # 此种百分比控制输出需掌握
+    print(f'{parameters[index]}: {100*f_score[index]:.2f}% ')
 
--- Machine_Learning/normalization.py
+++ Machine_Learning/normalization.py
@@ -5,14 +5,14 @@ import pandas as pd
 import csv
 from sklearn import preprocessing
 
-
-pd_data = pd.read_csv('parameters.csv')
+# read_csv方法默认会在开头加入新的unnamed列 设置index_col=0可以避免此现象
+pd_data = pd.read_csv('parameters.csv', index_col=0)
 norm = []
 # 可以将csv中带逗号的名称直接复制过来 此种寻找 名称对应列 的好处是 不会对csv中其它列(如分类标签等)造成影响
 # 注意 标签 同样需要归一化
 label = ["Culture serum(FBS=0,horse =1)", "Stretching direction(Radial=0,Uniaxial=1)", "Cyclic or static stretching(Cyclic=0,static=1)", "Continuous(0) or intermittent(1)", "Amplitude (%) (3%=0,8%-9%, 10%=1,15%=2,20%=3)", "Effective stretching duration (h)", "Increased (1) or inhibited (0) differentiation"]
 for i in label:
-    # 根据列名 获取到这一列的数据
+    # 根据列名 获取到这一列的数据 // x[:,i]为取所有行的第 i 个数据
     y = pd_data.loc[:, i]
     ys = list(preprocessing.scale(y))  # 归一化
     # norm为嵌套列表 外层列表数为 变量数 内层列表数为 每个变量的数据条目数
